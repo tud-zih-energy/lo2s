7c272dbd866900c41a04e8e076db7040f13187c6
Christian von Elm <christian.von_elm@mailbox.tu-dresden.de>
2018-06-06 12:06:21 +0200

    Split up locations_ into different location types

--- include/lo2s/trace/trace.hpp
+++ include/lo2s/trace/trace.hpp
@@ -94,7 +94,7 @@ public:
     otf2::writer::local& sample_writer(pid_t pid, pid_t tid);
     otf2::writer::local& cpu_writer(int cpuid);
     otf2::writer::local& metric_writer(pid_t pid, pid_t tid);
-    otf2::writer::local& metric_writer(const std::string& name);
+    otf2::writer::local& metric_writer(const std::string& name, pid_t pid = METRIC_PID);
 
     otf2::definition::metric_member metric_member(const std::string& name,
                                                   const std::string& description,
@@ -157,7 +157,7 @@ private:
 
     otf2::definition::location::reference_type location_ref() const
     {
-        return locations_.size();
+        return thread_locations_.size() + cpu_locations_.size() + named_locations_.size();
     }
 
     otf2::definition::location_group::reference_type location_group_ref() const
@@ -249,7 +249,9 @@ private:
     std::map<int, otf2::definition::location_group> location_groups_cpu_;
 
     // TODO add location groups (processes), read path from /proc/self/exe symlink
-    otf2::definition::container<otf2::definition::location> locations_;
+    std::map<pid_t, otf2::definition::location> thread_locations_;
+    std::map<int, otf2::definition::location> cpu_locations_;
+    std::map<std::string, otf2::definition::location> named_locations_;
 
     std::map<LineInfo, otf2::definition::source_code_location> source_code_locations_;
     std::map<LineInfo, otf2::definition::region> regions_line_info_;
