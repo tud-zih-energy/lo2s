17bf5781b6a99bc637921ca2f283171d72f29a06
Philipp Joram <mail@phijor.me>
2018-07-13 11:56:27 +0200

    Add cpu-cycles as possible metric leader event

--- src/perf/event_provider.cpp
+++ src/perf/event_provider.cpp
@@ -259,6 +259,7 @@ const CounterDescription& EventProvider::get_default_metric_leader_event()
     Log::debug() << "checking for metric leader event...";
     for (auto candidate : {
              "ref-cycles",
+             "cpu-cycles",
              "bus-cycles",
          })
     {
