3c7af44b5ad5a5a9881e6e862583aec3301ebb1f
Philipp Joram <mail@phijor.me>
2018-09-10 23:02:37 +0200

    Refactor ProcessController to be slightly more readable
    * use typesafe ptrace_<foo>(...) methods instead of using
        ptrace(PTRACE_<FOO>, ...) directly
    * add ThreadToProcessMapping class that replaces the plain 'tid -> pid'
        map, hopefully increasing readability of thread ID management
    * misc fixes: use an uniform error/debug message format, add some
        explanatory comments, etc.

--- include/lo2s/process_controller.hpp
+++ include/lo2s/process_controller.hpp
@@ -2,7 +2,7 @@
  * This file is part of the lo2s software.
  * Linux OTF2 sampling
  *
- * Copyright (c) 2016,
+ * Copyright (c) 2016-2018,
  *    Technische Universitaet Dresden, Germany
  *
  * lo2s is free software: you can redistribute it and/or modify
@@ -33,6 +33,52 @@ extern "C"
 
 namespace lo2s
 {
+/** A container mapping thread IDs to the PIDs of the processes a thread was
+ *  launched in.  In the language of the Linux kernel, this container maps
+ *  process identifiers (PIDs) to the corresponding task group IDs (TGIDs).
+ *  When referring to 'main thread', we are talking about the thread in a
+ *  process where tid == pid.
+ **/
+class ThreadToProcessMapping
+{
+public:
+    ThreadToProcessMapping() = default;
+
+    /** Add a the main thread of a process by PID.
+     *
+     *  This is equivalent to add_thread_to_process(\p pid, \p pid), as a
+     *  process is just a single thread where tid == pid.
+     **/
+    void add_main_thread_for_process(pid_t pid)
+    {
+        tid_to_pid_.emplace(pid, pid);
+    }
+
+    /** Add a thread identified by \p tid to the thread group of \p pid.
+     **/
+    void add_thread_to_process(pid_t tid, pid_t pid)
+    {
+        tid_to_pid_.emplace(tid, pid);
+    }
+
+    /** Retrieve the PID of the process that thread \p tid is contained in.
+     **/
+    pid_t get_process_for_thread(pid_t tid) const
+    {
+        return tid_to_pid_.at(tid);
+    }
+
+    /** Remove thread \p tid from the mapping.
+     **/
+    void remove_thread(pid_t tid)
+    {
+        tid_to_pid_.erase(tid);
+    }
+
+private:
+    std::map<pid_t, pid_t> tid_to_pid_;
+};
+
 class ProcessController
 {
 public:
@@ -53,6 +99,8 @@ private:
     monitor::AbstractProcessMonitor& monitor_;
     std::size_t num_wakeups_;
 
-    std::map<pid_t, pid_t> threads_;
+    ThreadToProcessMapping watched_threads_; //!< Keep track of which threads we
+                                             //!< are watching via ptrace and to
+                                             //!< which processes they belong
 };
 } // namespace lo2s
