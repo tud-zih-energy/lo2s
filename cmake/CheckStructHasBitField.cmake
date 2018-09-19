5deab6748bbe524d122398bcfaa3117fbc7ea04f
Christian von Elm <christian.von_elm@mailbox.tu-dresden.de>
2018-09-19 13:01:21 +0200

    Pre select right ContextSwitch sampling mode
    The desired mode can be forced with USE_PERF_RECORD_SWITCH
    To accomplish this check_struct_has_bitfield was added, which unlike
    check_struct_has_member works on bitfields

---
+++ cmake/CheckStructHasBitField.cmake
@@ -0,0 +1,20 @@
+include(CheckCSourceCompiles)
+
+macro(CHECK_STRUCT_HAS_BITFIELD _STRUCT _MEMBER _HEADER _RESULT)
+    set(_INCLUDE_FILES)
+    foreach(it ${_HEADER})
+        string(APPEND _INCLUDE_FILES "#include <${it}>\n")
+    endforeach()
+
+    set(_CHECK_STRUCT_MEMBER_SOURCE_CODE "
+    ${_INCLUDE_FILES}
+    int main()
+    {
+        ${_STRUCT} test;
+        test.${_MEMBER} = 0;
+        return 0;
+    }
+    ")
+
+    CHECK_C_SOURCE_COMPILES("${_CHECK_STRUCT_MEMBER_SOURCE_CODE}" ${_RESULT})
+endmacro()
