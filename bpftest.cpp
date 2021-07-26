e185b65e266ae7590be3e3e766a2ff01054b5368
Christian von Elm <cvonelm@cvonelm.de>
2021-07-26 12:01:31 +0200

    Now a complete, usable example

--- bpftest.cpp
+++ bpftest.cpp
@@ -6,57 +6,107 @@
  * Then compile this little program as: g++ bpftest.cpp -o bpftest -lbcc
  */
 
-#define PY_BIN "path/to/your/dtrace/python/binary/here"
+#define PY_BIN "/home/cvonelm/projects/cpython/python"
 #include <functional>
 #include <iostream>
 #include <signal.h>
 #include <vector>
 
 #include <bcc/BPF.h>
+#include <bpf/libbpf.h>
 
 const std::string BPF_PROGRAM = R"(
 #include <linux/sched.h>
+
 #include <uapi/linux/ptrace.h>
 struct event_t {
-	char name[16];
+    bool type;
+    int cpu;
+    u64 time;
+	char filename[32];
+    char funcname[32];
 };
-BPF_PERF_OUTPUT(events);
+BPF_RINGBUF_OUTPUT(events, 8);
 int on_function__entry(struct pt_regs *ctx) {
   struct event_t event = {};
-  char *usr_ptr;
-  bpf_usdt_readarg(2, ctx, &usr_ptr);
-  bpf_probe_read_user_str(event.name, 16, usr_ptr);
-  
-  events.perf_submit(ctx, &event, sizeof(event));
+  char *filename_ptr;
+  char *funcname_ptr;
+  event.type = false;
+  bpf_usdt_readarg(1, ctx, &filename_ptr);
+  bpf_usdt_readarg(2, ctx, &funcname_ptr);
+  bpf_probe_read_user_str(event.filename, 32, filename_ptr);
+  bpf_probe_read_user_str(event.funcname, 32, funcname_ptr);
+  event.time = bpf_ktime_get_ns();
+  event.cpu = ((struct task_struct *)bpf_get_current_task())->cpu;
+  events.ringbuf_output(&event, sizeof(event), 0);
+  return 0;
+}
+
+int on_function__return(struct pt_regs *ctx) {
+  struct event_t event = {};
+  char *filename_ptr;
+  char *funcname_ptr;
+  event.type = true;
+  bpf_usdt_readarg(1, ctx, &filename_ptr);
+  bpf_usdt_readarg(2, ctx, &funcname_ptr);
+  bpf_probe_read_user_str(event.filename, 32, filename_ptr);
+  bpf_probe_read_user_str(event.funcname, 32, funcname_ptr);
+  event.time = bpf_ktime_get_ns();
+  event.cpu = ((struct task_struct *)bpf_get_current_task())->cpu;
+  events.ringbuf_output(&event, sizeof(event), 0);
   return 0;
 }
 )";
 
 struct event_t
 {
-    char name[16];
+    bool type;
+    int cpu;
+    uint64_t time;
+    char filename[32];
+    char funcname[16];
 };
 
-void handle_output(void* cb_cookie, void* data, int data_size)
+int ident = 0;
+int handle_output(void* ctx, void* data, size_t data_size)
 {
+
     auto event = static_cast<event_t*>(data);
-    std::cout << "func name: " << event->name << std::endl;
-}
+    if (event->type == false) // ENTER
+    {
+        for (int i = 0; i < ident; i++)
+        {
+            std::cout << " ";
+        }
+        std::cout << "ENTER: (" << event->time << "):" << event->filename << "." << event->funcname
+                  << "(ON CPU:" << event->cpu << ")" << std::endl;
+        ident++;
+    }
+    else
+    {
+        ident--;
+        if (ident < 0)
+        {
+            ident = 0;
+        }
 
-std::function<void(int)> shutdown_handler;
-
-void signal_handler(int s)
-{
-    shutdown_handler(s);
+        for (int i = 0; i < ident; i++)
+        {
+            std::cout << " ";
+        }
+        std::cout << "LEAVE: (" << event->time << "):" << event->filename << "." << event->funcname
+                  << std::endl;
+    }
+    return 0;
 }
 
 int main(int argc, char** argv)
 {
-    ebpf::USDT u(PY_BIN, "python", "function__entry", "on_function__entry");
-
+    ebpf::USDT u1(PY_BIN, "python", "function__entry", "on_function__entry");
+    ebpf::USDT u2(PY_BIN, "python", "function__return", "on_function__return");
     ebpf::BPF* bpf = new ebpf::BPF();
 
-    auto init_res = bpf->init(BPF_PROGRAM, {}, { u });
+    auto init_res = bpf->init(BPF_PROGRAM, {}, { u1, u2 });
     if (init_res.code() != 0)
     {
         std::cerr << init_res.msg() << std::endl;
@@ -69,32 +119,24 @@ int main(int argc, char** argv)
         std::cerr << attach_res.msg() << std::endl;
         return 1;
     }
-    else
+
+    int err = 0;
+    struct ring_buffer* rb = NULL;
+
+    rb = ring_buffer__new(bpf->get_table("events").get_fd(), handle_output, NULL, NULL);
+    if (!rb)
     {
-        std::cout << "Attached to USDT " << u;
+        err = -1;
+        fprintf(stderr, "Failed to create ring buffer\n");
+        goto cleanup;
     }
 
-    auto open_res = bpf->open_perf_buffer("events", &handle_output);
-    if (open_res.code() != 0)
+    while (true)
     {
-        std::cerr << open_res.msg() << std::endl;
-        return 1;
+        ring_buffer__poll(rb, 100 /* timeout, ms */);
     }
+cleanup:
+    ring_buffer__free(rb);
 
-    shutdown_handler = [&](int s) {
-        std::cerr << "Terminating..." << std::endl;
-        bpf->detach_usdt_all();
-        delete bpf;
-        exit(0);
-    };
-    signal(SIGINT, signal_handler);
-
-    std::cout << "Started tracing, hit Ctrl-C to terminate." << std::endl;
-    auto perf_buffer = bpf->get_perf_buffer("events");
-    if (perf_buffer)
-        while (true)
-            // 100ms timeout
-            perf_buffer->poll(100);
-
-    return 0;
+    return err < 0 ? -err : 0;
 }
