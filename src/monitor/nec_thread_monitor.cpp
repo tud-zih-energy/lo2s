a044508a809e64fc24f5acac0e6fb91860f1daa9
Christian von Elm <christian.von_elm@tu-dresden.de>
2024-02-05 10:49:52 +0100

    Add support for NEC SX-Aurora counters
    Besides sampling, there are several performance monitor counters that we
    can read, such as L1 cache misses.
    
    This closes #237 as sampling and PMCs are probably the two reasonable
    things we can read from lo2s.

--- src/monitor/nec_thread_monitor.cpp
+++ src/monitor/nec_thread_monitor.cpp
@@ -19,15 +19,17 @@
  * along with lo2s.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <chrono>
 #include <lo2s/monitor/nec_thread_monitor.hpp>
+
+#include <chrono>
+
+#include <cassert>
 
 extern "C"
 {
+#include <libved.h>
 #include <veosinfo/veosinfo.h>
 }
-
-#include <libved.h>
 
 namespace lo2s
 {
@@ -36,6 +38,7 @@ namespace nec
 NecThreadMonitor::NecThreadMonitor(Thread thread, trace::Trace& trace, NecDevice device)
 : PollMonitor(trace, fmt::format("VE{} {}", device, thread.as_pid_t()),
               std::chrono::duration_cast<std::chrono::nanoseconds>(config().nec_read_interval)),
+  perf::counter::MetricWriter(MeasurementScope::nec_metric(thread.as_scope()), trace),
   nec_read_interval_(config().nec_read_interval), otf2_writer_(trace.nec_writer(device, thread)),
   nec_thread_(thread), trace_(trace), device_(device), cctx_manager_(trace)
 {
@@ -45,10 +48,19 @@ NecThreadMonitor::NecThreadMonitor(Thread thread, trace::Trace& trace, NecDevice
 
 void NecThreadMonitor::monitor([[maybe_unused]] int fd)
 {
-    static int reg[] = { VE_USR_IC };
-    uint64_t val;
+    static int reg[] = {
+        VE_USR_PMC00, VE_USR_PMC01, VE_USR_PMC02, VE_USR_PMC03, VE_USR_PMC04, VE_USR_PMC05,
+        VE_USR_PMC06, VE_USR_PMC07, VE_USR_PMC08, VE_USR_PMC09, VE_USR_PMC10, VE_USR_PMC11,
+        VE_USR_PMC12, VE_USR_PMC13, VE_USR_PMC14, VE_USR_PMC15, VE_USR_IC,
+    };
 
-    auto ret = ve_get_regvals(device_.as_int(), nec_thread_.as_pid_t(), 1, reg, &val);
+    constexpr size_t num_counters = sizeof(reg) / sizeof(int);
+
+    uint64_t val[num_counters];
+
+    assert(reg[num_counters - 1] == VE_USR_IC);
+
+    auto ret = ve_get_regvals(device_.as_int(), nec_thread_.as_pid_t(), num_counters, reg, val);
 
     if (ret == -1)
     {
@@ -57,8 +69,18 @@ void NecThreadMonitor::monitor([[maybe_unused]] int fd)
     }
 
     otf2::chrono::time_point tp = lo2s::time::now();
-    otf2_writer_.write_calling_context_sample(tp, cctx_manager_.sample_ref(val), 2,
-                                              trace_.nec_interrupt_generator().ref());
+    otf2_writer_.write_calling_context_sample(tp, cctx_manager_.sample_ref(val[num_counters - 1]),
+                                              2, trace_.nec_interrupt_generator().ref());
+
+    metric_event_.timestamp(tp);
+
+    otf2::event::metric::values& values = metric_event_.raw_values();
+
+    for (size_t i = 0; i < num_counters - 1; i++)
+    {
+        values[i] = val[i];
+    }
+    writer_.write(metric_event_);
 }
 
 void NecThreadMonitor::finalize_thread()
