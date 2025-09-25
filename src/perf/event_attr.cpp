ddcad294efb1d332b2a812fd3cf647f63a4b7463
Christian von Elm <christian.von_elm@tu-dresden.de>
2025-09-25 15:48:12 +0200

    feat(perf): Add support for pmu-events
    This adds support for pmu-events from the Linux kernel, as discussed
    in issue 379.
    
    "pmu-events" is the folder on the Linux kernel source code that contains
    the list of all in-built events for the perf command. In
    github.com/cvonelm/pmu-events (should probably be moved to
    tud-zih-energy)
    I've separated the code from the kernel and made it externally usable.
    
    This commit adds the ability to get event names via pmu-events to lo2s.

--- src/perf/event_attr.cpp
+++ src/perf/event_attr.cpp
@@ -22,13 +22,11 @@
 #include <lo2s/build_config.hpp>
 #include <lo2s/perf/event_attr.hpp>
 #include <lo2s/perf/event_resolver.hpp>
-
+#include <lo2s/perf/pmu.hpp>
 #include <lo2s/topology.hpp>
 #include <lo2s/util.hpp>
 
 #include <nitro/lang/string.hpp>
-
-#include <optional>
 
 extern "C"
 {
@@ -41,7 +39,7 @@ namespace lo2s
 namespace perf
 {
 
-std::set<Cpu> get_cpu_set_for(EventAttr ev)
+std::set<Cpu> EventAttr::get_cpu_set_for(EventAttr ev)
 {
     std::set<Cpu> cpus = std::set<Cpu>();
 
@@ -59,78 +57,6 @@ std::set<Cpu> get_cpu_set_for(EventAttr ev)
     }
 
     return cpus;
-}
-
-template <typename T>
-std::optional<T> try_read_file(const std::string& filename)
-{
-    T val;
-    std::ifstream stream(filename);
-    stream >> val;
-    if (stream.fail())
-    {
-        return {};
-    }
-    return val;
-}
-
-static std::uint64_t parse_bitmask(const std::string& format)
-{
-    enum BITMASK_REGEX_GROUPS
-    {
-        BM_WHOLE_MATCH,
-        BM_BEGIN,
-        BM_END,
-    };
-
-    std::uint64_t mask = 0x0;
-
-    static const std::regex bit_mask_regex(R"((\d+)?(?:-(\d+)))");
-    const std::sregex_iterator end;
-    for (std::sregex_iterator i = { format.begin(), format.end(), bit_mask_regex }; i != end; ++i)
-    {
-        const auto& match = *i;
-        int start = std::stoi(match[BM_BEGIN]);
-        int end = (match[BM_END].length() == 0) ? start : std::stoi(match[BM_END]);
-
-        const auto len = (end + 1) - start;
-        if (start < 0 || end > 63 || len > 64)
-        {
-            throw EventAttr::InvalidEvent("invalid config mask");
-        }
-
-        /* Set `len` bits and shift them to where they should start.
-         * 4-bit example: format "1-3" produces mask 0b1110.
-         *    start := 1, end := 3
-         *    len  := 3 + 1 - 1 = 3
-         *    bits := bit(3) - 1 = 0b1000 - 1 = 0b0111
-         *    mask := 0b0111 << 1 = 0b1110
-         * */
-
-        // Shifting by 64 bits causes undefined behaviour, so in this case set
-        // all bits by assigning the maximum possible value for std::uint64_t.
-        const std::uint64_t bits =
-            (len == 64) ? std::numeric_limits<std::uint64_t>::max() : (1ull << len) - 1;
-
-        mask |= bits << start;
-    }
-    Log::debug() << std::showbase << std::hex << "config mask: " << format << " = " << mask
-                 << std::dec << std::noshowbase;
-    return mask;
-}
-
-static constexpr std::uint64_t apply_mask(std::uint64_t value, std::uint64_t mask)
-{
-    std::uint64_t res = 0;
-    for (int mask_bit = 0, value_bit = 0; mask_bit < 64; mask_bit++)
-    {
-        if (mask & (1ull << mask_bit))
-        {
-            res |= ((value >> value_bit) & (1ull << 0)) << mask_bit;
-            value_bit++;
-        }
-    }
-    return res;
 }
 
 EventAttr::EventAttr(const std::string& name, perf_type_id type, std::uint64_t config,
@@ -155,37 +81,6 @@ PredefinedEventAttr::PredefinedEventAttr(const std::string& name, perf_type_id t
     cpus_ = get_cpu_set_for(*this);
 
     event_is_openable();
-}
-
-void EventAttr::event_attr_update(std::uint64_t value, const std::string& format)
-{
-    // Parse config terms //
-
-    /* Format:  <term>:<bitmask>
-     *
-     * We only assign the terms 'config' and 'config1'.
-     *
-     * */
-
-    static constexpr auto npos = std::string::npos;
-    const auto colon = format.find_first_of(':');
-    if (colon == npos)
-    {
-        throw EventAttr::InvalidEvent("invalid format description: missing colon");
-    }
-
-    const auto target_config = format.substr(0, colon);
-    const auto mask = parse_bitmask(format.substr(colon + 1));
-
-    if (target_config == "config")
-    {
-        attr_.config |= apply_mask(value, mask);
-    }
-
-    if (target_config == "config1")
-    {
-        attr_.config1 |= apply_mask(value, mask);
-    }
 }
 
 void EventAttr::sample_period(int period)
@@ -570,56 +465,24 @@ SysfsEventAttr::SysfsEventAttr(const std::string& ev_name)
         pmu_path = std::filesystem::path();
     }
 
-    name_ = ev_name_match[2];
-    std::string pmu_name = ev_name_match[1];
-    pmu_path = std::filesystem::path("/sys/bus/event_source/devices") / pmu_name;
+    PMU pmu(ev_name_match[1]);
+    std::string specific_event = ev_name_match[2];
 
-    Log::debug() << "parsing event description: pmu='" << pmu_name << "', event='" << name_ << "'";
+    Log::debug() << "parsing event description: pmu='" << pmu.name() << "', event='" << name_
+                 << "'";
 
-    // read PMU type id
-    auto type = try_read_file<std::underlying_type<perf_type_id>::type>(pmu_path / "type");
+    struct perf_event_attr pmu_attr = pmu.ev_from_name(specific_event);
 
-    if (!type.has_value())
-    {
-        using namespace std::string_literals;
-        throw EventAttr::InvalidEvent("unknown PMU '"s + pmu_name + "'");
-    }
+    attr_.type = pmu.type();
+    attr_.config = pmu_attr.config;
+    attr_.config1 = pmu_attr.config1;
 
-    attr_.type = static_cast<perf_type_id>(type.value());
-    attr_.config = 0;
-    attr_.config1 = 0;
+    Log::debug() << std::hex << std::showbase << "parsed event description: " << pmu.name() << "/"
+                 << name_ << "/type=" << attr_.type << ",config=" << attr_.config
+                 << ",config1=" << attr_.config1 << std::dec << std::noshowbase << "/";
 
-    // Parse event configuration from sysfs //
-
-    // read event configuration
-    std::filesystem::path event_path = pmu_path / "events" / name_;
-    auto ev_cfg = try_read_file<std::string>(event_path);
-    if (!ev_cfg.has_value())
-    {
-        using namespace std::string_literals;
-        throw EventAttr::InvalidEvent("unknown event '"s + name_ + "' for PMU '"s + pmu_name + "'");
-    }
-
-    name_ = ev_name;
-
-    /* Event configuration format:
-     *   One or more terms with optional values, separated by ','.  (Taken from
-     *   linux/Documentation/ABI/testing/sysfs-bus-event_source-devices-events):
-     *
-     *     <term>[=<value>][,<term>[=<value>]...]
-     *
-     *   Example (config for 'cpu/cache-misses' on an Intel Core i5-7200U):
-     *
-     *     event=0x2e,umask=0x41
-     *
-     *  */
-
-    enum EVENT_CONFIG_REGEX_GROUPS
-    {
-        EC_WHOLE_MATCH,
-        EC_TERM,
-        EC_VALUE,
-    };
+    scale(pmu.scale(specific_event));
+    unit(pmu.unit(specific_event));
 
     // If the processor is heterogenous, "cpus" contains the cores that support this PMU. If the
     // PMU is an uncore PMU "cpumask" contains the cores that are logically assigned to that
@@ -641,42 +504,6 @@ SysfsEventAttr::SysfsEventAttr(const std::string& ev_name)
         std::transform(cpuids.begin(), cpuids.end(), std::inserter(cpus_, cpus_.end()),
                        [](uint32_t cpuid) { return Cpu(cpuid); });
     }
-
-    static const std::regex kv_regex(R"(([^=,]+)(?:=([^,]+))?)");
-
-    Log::debug() << "parsing event configuration: " << ev_cfg.value();
-    std::smatch kv_match;
-    while (std::regex_search(ev_cfg.value(), kv_match, kv_regex))
-    {
-        static const std::string default_value("0x1");
-
-        const std::string& term = kv_match[EC_TERM];
-        const std::string& value =
-            (kv_match[EC_VALUE].length() != 0) ? kv_match[EC_VALUE] : default_value;
-
-        auto format = try_read_file<std::string>(pmu_path / "format" / term);
-        if (!format.has_value())
-        {
-            throw EventAttr::InvalidEvent("cannot read event format");
-        }
-
-        static_assert(sizeof(std::uint64_t) >= sizeof(unsigned long),
-                      "May not convert from unsigned long to uint64_t!");
-
-        std::uint64_t val = std::stol(value, nullptr, 0);
-        Log::debug() << "parsing config assignment: " << term << " = " << std::hex << std::showbase
-                     << val << std::dec << std::noshowbase;
-        event_attr_update(val, format.value());
-
-        ev_cfg = kv_match.suffix();
-    }
-
-    Log::debug() << std::hex << std::showbase << "parsed event description: " << pmu_name << "/"
-                 << name_ << "/type=" << attr_.type << ",config=" << attr_.config
-                 << ",config1=" << attr_.config1 << std::dec << std::noshowbase << "/";
-
-    scale(try_read_file<double>(event_path.replace_extension(".scale")).value_or(1.0));
-    unit(try_read_file<std::string>(event_path.replace_extension(".unit")).value_or("#"));
 
     if (!event_is_openable())
     {
