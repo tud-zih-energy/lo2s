d9d1fb49cab5c8c7cd54d8917003c751d5fdc1dd
Christian von Elm <christian.von_elm@tu-dresden.de>
2025-09-25 10:48:55 +0200

    fix(cmake): Renovate CMake stuffs
    - Create PkgConfigWrapper, which wraps pkg_check_modules with
      correct handling of static libraries
    - Use pkg-config for every package where it is possible. pkg-config
      _usually_ knows reasonably well about the static library dependencies.
      Getting a hold of everything that is required by some libfoo.a is
      otherwise maddening.
        - Port Radare to pkg-config
        - Port libaudit to pkg-config
        - Port libbpf to pkg-config
    - Sync FindBpfObject with upstream
    - If fully static build is selected, don't build the shared
      injection library.
    - Delete FindBinutils, we have used elfutils for quite some time now.

--- cmake/FindLibBpf.cmake
+++ cmake/FindLibBpf.cmake
@@ -1,62 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
 option(LibBpf_USE_STATIC_LIBS "Link LibBpf statically." ON)
 
-find_package(PkgConfig)
-find_package(LibDw)
+include(${CMAKE_CURRENT_LIST_DIR}/PkgConfigWrapper.cmake)
 
-if(NOT LibDw_FOUND)
-    return()
-endif()
-
-if(NOT PKG_CONFIG_FOUND)
-    return()
-endif()
-
-pkg_check_modules(LIBBPF_PKG_CONFIG libbpf)
-
-if(NOT LIBBPF_PKG_CONFIG_FOUND)
-    return()
-endif()
-
-find_path(LIBBPF_INCLUDE_DIRS
-  NAMES
-    bpf/bpf.h
-    bpf/btf.h
-    bpf/libbpf.h
-    PATHS
-    ${LIBBPF_PKG_CONFIG_INCLUDDE_DIRS}
-    ENV CPATH)
-
-
-if(LibBpf_USE_STATIC_LIBS)
-find_library(LIBBPF_LIBRARIES
-  NAMES
-    libbpf.a
-    HINTS
-    ${LIBBPF_PKG_CONFIG_STATIC_LIBRARY_DIRS}
-    ENV LIBRARY_PATH
-    )
-else()
-find_library(LIBBPF_LIBRARIES
-  NAMES
-    libbpf.so
-    HINTS
-    ${LIBBPF_PKG_CONFIG_LIBRARY_DIRS}
-    ENV LIBRARY_PATH
-    ENV LD_LIBRARY_PATH)
-endif()
-
-include (FindPackageHandleStandardArgs)
-
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibBpf DEFAULT_MSG
-  LIBBPF_LIBRARIES
-  LIBBPF_INCLUDE_DIRS)
-
-
-if(LibBpf_FOUND)
-    add_library(LibBpf::LibBpf UNKNOWN IMPORTED)
-    set_property(TARGET LibBpf::LibBpf PROPERTY IMPORTED_LOCATION ${LIBBPF_LIBRARIES})
-    target_link_libraries(LibBpf::LibBpf INTERFACE LibDw::LibDw)
-    target_include_directories(LibBpf::LibBpf INTERFACE ${LIBBPF_INCLUDE_DIRS})
-endif()
-mark_as_advanced(LIBBPF_INCLUDE_DIRS LIBBPF_LIBRARIES)
+FindPkgConfigWrapper(LibBpf libbpf bpf)
--- cmake/test_static_radare.c
+++
@@ -1,6 +0,0 @@
-#include <libr/r_lib.h>
-
-int main(void)
-{
-    r_lib_path("foo");
-}
